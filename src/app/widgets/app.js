e5368f729b37aaa4706744e443add30ce8f61a1a
Jack Pedler <jpedler96@outlook.com>
2025-06-01 11:12:33 +1000

    add spinning loading icon

--- src/app/widgets/app.js
+++ src/app/widgets/app.js
@@ -42,7 +42,13 @@ function Results({city}) {
 
 
     if (isLoading) {
-        return <div>Loading...</div>;
+        return (
+            <div id="loading-container">
+                <svg id="loading-icon" style={{ transform: 'rotate(45deg)' }} xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 512 512">
+                    <path d="M304 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 416a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM48 304a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z"/>
+                </svg>
+            </div>
+        );
     }
 
 
